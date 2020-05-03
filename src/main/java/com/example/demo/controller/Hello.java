86f10d17855e7e6ac2119757fa1fbc153fa614de
xiaopang6666 <1076902468@qq.com>
2020-05-03 21:29:16 +0800

    配置

---
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class Hello {
+
+    @RequestMapping("hello")
+    public List<String> h(){
+        ArrayList<String> result = new ArrayList<>();
+        result.add("qwe");
+        result.add("www");
+        System.out.println(66);
+        return result;
+    }
+}
