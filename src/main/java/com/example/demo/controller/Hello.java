8f6dcc82f0a3428cd71d4893f460096c67f0dc15
xiaopang6666 <62560848+xiaopang6666@users.noreply.github.com>
2020-05-04 10:11:42 +0800

    Update Hello.java

--- src/main/java/com/example/demo/controller/Hello.java
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -16,7 +16,7 @@ public class Hello {
         ArrayList<String> result = new ArrayList<>();
         result.add("qwe");
         result.add("www");
-        System.out.println(6678);
+        System.out.println(66789);
         //ssss
         //ww
         return result;
